9814d8a70f78f94a5b16e530d7b0808962580a11
Delrue Antoine <145853853+TheArtoriass@users.noreply.github.com>
2024-03-21 16:47:24 +0100

    meteotexte import

--- lib/routes/home_route.dart
+++ lib/routes/home_route.dart
@@ -3,7 +3,7 @@ import 'package:weather/common/weatherText.dart';
 
 class MenuRoute extends StatelessWidget {
   const MenuRoute({Key? key}) : super(key: key);
-  final int heure = 11;
+  final int heure = 14;
   final int minute = 30;
 
   LinearGradient determineBackgroundColor() {
@@ -39,32 +39,34 @@ class MenuRoute extends StatelessWidget {
                 gradient: determineBackgroundColor(),
               )
           ),
-          Positioned(
-            bottom: -200,
-            left: MediaQuery.of(context).size.width / 800,
-            child: SizedBox(
-              height: 800.0, // Définir la hauteur
-              width: MediaQuery.of(context).size.width * 1, // Définir la largeur
-              child: Image.asset(
-                'assets/montagne2.jpg',
-                fit: BoxFit.fitHeight,
+          if (heure < 16)
+            Positioned(
+              bottom: -200,
+              left: MediaQuery.of(context).size.width / 800,
+              child: SizedBox(
+                height: 800.0, // Définir la hauteur
+                width: MediaQuery.of(context).size.width * 1, // Définir la largeur
+                child: Image.asset(
+                  'assets/montagne2.jpg',
+                  fit: BoxFit.fitHeight,
 
+                ),
               ),
             ),
-          ),
-          Positioned(
-            bottom: -200,
-            left: MediaQuery.of(context).size.width / 800,
-            child: SizedBox(
-              height: 600.0, // Définir la hauteur
-              width: MediaQuery.of(context).size.width * 1, // Définir la largeur
-              child: Image.asset(
-                'assets/montagne2.jpg',
-                fit: BoxFit.fitHeight,
+          if (heure < 16)
+            Positioned(
+              bottom: -200,
+              left: MediaQuery.of(context).size.width / 800,
+              child: SizedBox(
+                height: 600.0, // Définir la hauteur
+                width: MediaQuery.of(context).size.width * 1, // Définir la largeur
+                child: Image.asset(
+                  'assets/montagne2.jpg',
+                  fit: BoxFit.fitHeight,
 
+                ),
               ),
             ),
-          ),
         ],
 
       ),
