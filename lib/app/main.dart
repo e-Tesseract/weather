2e9ba848b83c8344ddde97b372b51d2595189ad6
PIERRU Matheo <matheo.pierru@etu.univ.littoral.fr>
2024-03-21 15:20:59 +0100

    Pour faire page blanche

--- lib/app/main.dart
+++ lib/app/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather/routes/home_route.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
 
 
       ),
-      home: ,
+      home: const MenuRoute(),
 
     );
   }
---
+++ lib/routes/home_route.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MenuRoute extends StatelessWidget {
+  const MenuRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+
+    );
+  }
+}
