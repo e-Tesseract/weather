5e62c5629b0c6677be33afeaba8b86ac6bc7023e
Capon Hugo <capon.hugo@gmail.com>
2024-04-02 17:11:28 +0200

    maj villes

---
+++ lib/menu/parametres_route.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:weather/common/weatherMain.dart';
+
+
+
+class ParametresRoute extends StatelessWidget {
+  ParametresRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          Center(
+            child: Text('Ceci est une page de test', style: TextStyle(color: Colors.white70)),
+          ),
+          AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            // Changer la couleur de l'icon
+            iconTheme: const IconThemeData(color: Colors.white70, size: 35),
+            title: const Text('Paramètres', style: TextStyle(color: Colors.white70)),
+          ),
+        ],
+      ),
+    );
+  }
+}
